7f6f11a5c7ad769b7b68e1224e48179a9a222606
RenYuZhuo <renyuzhuo@foxmail.com>
2020-01-31 22:25:45 +0800

    [bug]Fix Repo Bug

--- src/pages/repo/repo.js
+++ src/pages/repo/repo.js
@@ -113,6 +113,7 @@ class Repo extends Component {
                 forks: project.forks_count,
                 license: project.license,
                 watch: project.subscribers_count,
+                issue: project.open_issues_count,
                 owner: {
                   login: project.owner.login,
                   type: project.owner.type
@@ -596,8 +597,8 @@ class Repo extends Component {
             <View className='list_title'>Issues</View>
             <View className='list_content'>
               {
-                repo.open_issues_count > 0 &&
-                <View className='tag'>{repo.open_issues_count}</View>
+                repo.issue > 0 &&
+                <View className='tag'>{repo.issue}</View>
               }
               <AtIcon prefixClass='ion' value='ios-arrow-forward' size='18' color='#7f7f7f' />
             </View>
