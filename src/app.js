27b0c43e61e27311a0cb48335743b1dd54b4c201
renyuzhuo <renyuzhuo@baidu.com>
2024-10-15 20:08:58 +0800

    Init

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+import { useLaunch } from '@tarojs/taro'
+
+import './app.scss'
+
+function App({ children }) {
+  useLaunch(() => {
+    console.log('App launched.')
+  })
+
+  // children 是将要会渲染的页面
+  return children
+}
+  
+
+
+export default App
